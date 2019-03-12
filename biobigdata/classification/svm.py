77eea4761f8ca09a3438167a284df378a2b671d5
VOIX <32408309+xiangsheng1325@users.noreply.github.com>
2019-03-12 15:28:28 +0800

    Update svm.py

--- biobigdata/classification/svm.py
+++ biobigdata/classification/svm.py
@@ -1,6 +1,9 @@
 from sklearn.svm import NuSVC
+from sklearn.metrics import accuracy_score
 
 def svm(train_feature, train_label):
-    clf = NuSVC(kernel='rbf', gamma='scale')
+    clf = NuSVC(kernel='rbf', gamma='scale', probability=True)
     clf.fit(train_feature, train_label)
-    return clf.score(train_feature,train_label)
+    _pre_label = clf.predict_proba(train_feature)
+    _score = clf.score(train_feature,train_label)
+    return accuracy_score(train_label, _pre_label)
